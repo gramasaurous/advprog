56d09adac5f4f7354121d99e34ec36dfae11ed40
Graham Greving <ggreving@ucsc.edu>
2014-07-26 00:00:16 -0700

    main mostly working, starting on listmap

--- asg3/code/main.cpp
+++ asg3/code/main.cpp
@@ -39,71 +39,81 @@ int scan_options (int argc, char** argv) {
    return c;
 }
 
-int do_line(string line) {
-   line = trim (line);
-   if (line.size() == 0 or line[0] == '#') {
-      cout << "Comment or blank line." << endl;
-      return 0;
-   }
-   size_t pos = line.find_first_of ("=");
-   if (pos == string::npos) {
-      //cout << "key   = \"" << line << "\"" << endl;
-      //cout << "No value in this line." << endl;
-      cout << "do_find(key)" << endl;
-   } else {
-      string key = trim (line.substr (0, pos == 0 ? 0 : pos - 1));
-      string value = trim (line.substr (pos + 1));
-      bool k = (key.size() == 0); // true if no key
-      bool v = (value.size() == 0); // true if no value
+void do_insert() {
 
-      //cout << "key   = \"" << key << "\"" << endl;
-      //cout << "value = \"" << value << "\"" << endl;
-      if (k == true && v == true) { // no key
-         cout << "do_printall()" << endl;
-      } else if (v == true) { // key, but no value
-         cout << "do_delete(key)" << endl;
-      } else {                // both value and key given
-         cout << "do_insert(key, value)" << endl;
-      }
-   }
-   return 0;
 }
 
-void do_file(const string& filename, istream& input) {
+void do_file(string filename, istream& input, str_str_map& m) {
    for (int lcount = 1;; ++lcount) {
       string line;
       getline(input, line);
       if (input.eof()) break;
       else {
-         do_line(line);
+         cout << filename << ": " << lcount << ": " << line << endl;
+         line = trim (line);
+         if (line.size() == 0 or line[0] == '#') {
+            cout << "Comment or blank line." << endl;
+            return;
+         }
+         size_t pos = line.find_first_of ("=");
+         if (pos == string::npos) {
+            //cout << "key   = \"" << line << "\"" << endl;
+            //cout << "No value in this line." << endl;
+            cout << "do_find(key)" << endl;
+         } else {
+            string key=trim (line.substr (0, pos == 0 ? 0 : pos - 1));
+            string value = trim (line.substr (pos + 1));
+            bool k = (key.size() == 0); // true if no key
+            bool v = (value.size() == 0); // true if no value
+
+            //cout << "key   = \"" << key << "\"" << endl;
+            //cout << "value = \"" << value << "\"" << endl;
+            if (k == true && v == true) { // no key
+               cout << "do_printall()" << endl;
+            } else if (v == true) { // key, but no value
+               cout << "do_delete(key)" << endl;
+            } else {                // both value and key given
+               cout << "do_insert(" << key << ", " << value
+                  << ")" << endl;
+               str_str_pair pair(key,value);
+               m.insert(pair);
+            }
+         }
       }
    }
 }
 
 int main (int argc, char** argv) {
    sys_info::set_execname (argv[0]);
-   int file_pos = scan_options (argc, argv);
 
-   str_str_map test;
-   for (int argi = file_pos; argi < argc; ++argi) {
-      string filename = argv[argi];
-      if (filename == "-") do_file(filename, cin);
-      else {
-         ifstream file_in(filename);
-         if (! file_in.fail()) do_file(filename, file_in);
-         else complain() << "file not found." << endl;
-      }
+   str_str_map m;
+   int file_pos = scan_options (argc, argv);
+   if (file_pos >= argc) {
+      do_file("-", cin, m);
+   } else {
+      for (int argi = file_pos; argi < argc; ++argi) {
+         string filename = argv[argi];
+         if (filename == "-") {
+            do_file("-", cin, m);
+         } else {
+            ifstream file_in(filename);
+            if (! file_in.fail()) {
+               do_file(filename, file_in, m);
+            } else complain() << "File:" << filename <<
+               " does not exist." << endl;
+         }
+      }      
    }
 
-   for (str_str_map::iterator itor = test.begin();
-        itor != test.end(); ++itor) {
+   //str_str_map test;
+   for (str_str_map::iterator itor = m.begin();
+        itor != m.end(); ++itor) {
       cout << "During iteration: " << *itor << endl;
    }
 
-   str_str_map::iterator itor = test.begin();
-   test.erase (itor);
+   str_str_map::iterator itor = m.begin();
+   m.erase (itor);
 
-   cout << "EXIT_SUCCESS" << endl;
    return EXIT_SUCCESS;
 }
 
