18d42e22a64f5a20f6bcd6330be209ea175cbe22
gramasaurous <ggreving@ucsc>
2014-08-04 19:49:16 -0700

    Fixed score files + some readme

--- asg4/.score/ellipse-etc.gd
+++ asg4/.score/ellipse-etc.gd
@@ -1,12 +1,10 @@
-# $Id: ellipse-etc.gd,v 1.1 2014-05-20 13:58:08-07 - - $
+# $Id: ellipse-etc.gd,v 1.2 2014-08-04 18:17:15-07 - - $
 # Test ellipse, polygon, and a few things.
 define first ellipse 200 400
 define second ellipse 400 200
-define third circle 100 100
+define third circle 100
 define poly polygon 0 0 10 -2 20 0 22 10 20 20 10 22 0 20 -2 10
-define ss polygon 0 100 100 100 0 0 100 0
 draw blue poly 50 50
 draw cyan first 300 300
 draw yellow second 300 300
 draw black third 300 300
-draw red ss 100 100
--- asg4/.score/rectilinear.gd
+++ asg4/.score/rectilinear.gd
@@ -1,16 +1,14 @@
-# $Id: rectilinear.gd,v 1.1 2014-05-20 14:01:32-07 - - $
+# $Id: rectilinear.gd,v 1.2 2014-08-04 18:15:54-07 - - $
 # Some rectilinear symbols
 define rect rectangle 100 50
 define squ square 75
 define card diamond 50 100
 define tri triangle 0 0 100 50 0 60
-define r_t right_triangle 50 100
 define iso isosceles 100 120
 define equi equilateral 100 
-draw red rect 100 100
+draw cyan rect 100 100
 draw cyan squ 200 100
-draw yellow card 300 100
+draw cyan card 300 100
 draw magenta tri 100 300
-draw PapayaWhip r_t 150 150
-draw LemonChiffon3 iso 200 300
+draw magenta iso 200 300
 draw magenta equi 300 300
